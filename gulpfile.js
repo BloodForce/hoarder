b9896255f48d21d8ac804a0b1c6aa2c7f41a8da1
Shepless <chris.shepherd1985@gmail.com>
2016-10-27 21:33:04 +0100

    More tidy up

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,22 @@ const nodemon = require('gulp-nodemon');
 const tsProject = tsc.createProject('tsconfig.json');
 
 gulp.task('compile-ts', () => {
-  return tsProject.src()
-    .pipe(tsProject())
-    .js.pipe(gulp.dest('build'));
+	return tsProject.src()
+		.pipe(tsProject())
+		.js.pipe(gulp.dest('build'));
 });
 
 gulp.task('clean-ts', (done) => {
-  return del(['build/**/*'], done);
+	return del(['build/**/*'], done);
 });
 
-gulp.task('watch', ['compile-ts'], function () {
-  nodemon({
-    script: './build/src/main.js',
-    ext: 'ts',
-    watch: './src',
-    tasks: ['compile-ts']
-  });
+gulp.task('watch', ['compile-ts'], () => {
+	nodemon({
+		script: './build/src/main.js',
+		ext: 'ts',
+		watch: './src',
+		tasks: ['compile-ts']
+	});
 });
 
 gulp.task('build', ['ts-lint', 'compile-ts']);
