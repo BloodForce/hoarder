447675c84dcf0139f7ac29ebba83cb7e393fe14e
Chris Shepherd <Shepless@users.noreply.github.com>
2016-10-31 08:57:32 +0000

    Update config.ts

--- src/plugins/torrent-providers/pirate-bay/config.ts
+++ src/plugins/torrent-providers/pirate-bay/config.ts
@@ -1,8 +1,6 @@
 const CONFIG = {
   searchUrl: "https://hd-torrents.org/torrents.php?search=",
-  rssUrl: "https://hd-torrents.org/rss.php",
-  uid: "42080",
-  pass: "38fbbb69f9a6be9df7db33a2b7da1eda"
+  rssUrl: "https://hd-torrents.org/rss.php"
 };
 
 export {CONFIG};
