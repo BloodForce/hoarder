c2a6b204e5d3be51557585113f01c8cfb35096d7
Shepless <chris.shepherd1985@gmail.com>
2016-10-31 22:07:40 +0000

    Added missing files

---
+++ src/library/index.ts
@@ -0,0 +1,6 @@
+import * as Orm from './orm';
+import * as Plugins from './plugin';
+import * as Seedboxes from './seed-box';
+
+export { Orm, Plugins, Seedboxes };
+
---
+++ src/library/orm/entities/index.ts
@@ -0,0 +1,6 @@
+import { IMovieEntity } from './movie';
+import { IPluginEntity } from './plugin';
+import { ISeedBoxEntity } from './seed-box';
+import { ITvShowEntity } from './tv-show';
+
+export { IMovieEntity, IPluginEntity, ISeedBoxEntity, ITvShowEntity }
